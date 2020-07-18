c8b00e5d3c2de8138de7e7a7fb6e220b6bb0aea4
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-07-18 15:48:38 +0300

    Dataaccess involved REPO

---
+++ GokceFramework/GokceFramework.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,14 @@
+﻿using GokceFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GokceFramework.Core.DataAccess
+{
+    public interface IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        IQueryable<T> Table { get; }
+    }
+}
